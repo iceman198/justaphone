2e7e4fd2af1bbb0773b32b2fe87a29b0da345178
Sean Dutton <iceman198@gmail.com>
2018-11-28 12:51:12 -0500

    nodejs oled

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var exec = require('child_process').exec;
+var oled = require('oled-ssd1306-i2c');
+var font = require('oled-font-5x7');
+
+var oled_opts = {
+  width: 128, // screen width
+  height: 32, // screen height
+  address: 0x3C, // Pass I2C address of screen if it is not the default of 0x3C
+  device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
+  microview: true, // set to true if you have a microview display
+};
+
+var oled = new oled(oled_opts);
 
 const express = require('express');
 //const url = require('url');
@@ -7,7 +19,7 @@ const port = 8088;
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+  res.sendFile(__dirname + "/" + "index.html");
 })
 
 app.get('/service/', (request, response) => {
@@ -19,13 +31,13 @@ app.get('/service/', (request, response) => {
     if (cmd == "shutdown") {
       console.log('sending command');
       response.sendStatus(`Shutdown initiated`);
-      dir = exec(`shutdown now`, function(err, stdout, stderr) {
+      dir = exec(`shutdown now`, function (err, stdout, stderr) {
         if (err) {
           console.log('error sending command: ', err);
         }
         console.log(stdout);
       });
-  
+
       dir.on('exit', function (code) {
         console.log('exit complete with code ', code);
       });
@@ -44,11 +56,16 @@ app.listen(port, (err) => {
 });
 
 function startup() {
-  dir = exec(`python /home/pi/Documents/JustAphone/python/stats.py > /dev/null 2>&1`, function(err, stdout, stderr) {
-    if (err) {
-      console.log('error sending command: ', err);
-    }
-  });
+  // dir = exec(`python /home/pi/Documents/justaphone/python/stats.py > /dev/null 2>&1`, function (err, stdout, stderr) {
+  //   if (err) {
+  //     console.log('error sending command: ', err);
+  //   }
+  // });
+  oled.turnOnDisplay();
+  oled.clearDisplay();
+  oled.setCursor(1, 1);
+  oled.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
+  oled.update();
 }
 
 startup();
