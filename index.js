f25269a8554af9855a1f10e8bbf4039d2a30bf66
Sean Dutton <iceman198@gmail.com>
2018-11-29 09:19:58 -0500

    m

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var exec = require('child_process').exec;
-var oled = require('oled-js-pi'); // https://github.com/juddflamm/oled-js-pi
+var oled = require('oled-ssd1306-i2c'); // https://github.com/perjg/oled_ssd1306_i2c
 var font = require('oled-font-5x7');
 
 var oled_opts = {
   width: 128, // screen width
   height: 32, // screen height
-  address: 0x3C // Pass I2C address of screen if it is not the default of 0x3C
+  address: 0x3C, // Pass I2C address of screen if it is not the default of 0x3C
+  device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
+  microview: true, // set to true if you have a microview display
 };
 
 var oled = new oled(oled_opts);
