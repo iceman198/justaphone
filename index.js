99db39c38111cab0f8926bc9d75f0b6506c5618e
Sean Dutton <iceman198@gmail.com>
2018-11-29 09:08:57 -0500

    m

--- index.js
+++ index.js
@@ -77,9 +77,9 @@ function writeOled(line1, line2, line3) {
   clearDisplay();
   var text = `${line1}\n${line2}\n${line3}`;
   oled.setCursor(1, 1);
-  oled.writeString(font, 1, line1, 1, false);
-  oled.writeString(font, 1, line2, 1, false);
-  oled.writeString(font, 1, line3, 1, false);
+  oled.writeString(font, 1, text, 1, false);
+  //oled.writeString(font, 1, line2, 1, false);
+  //oled.writeString(font, 1, line3, 1, false);
   oled.update();
 }
 
