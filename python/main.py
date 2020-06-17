3599aba381be452255ad8de6ff7f27df1a18418a
Sean Dutton <iceman198@gmail.com>
2020-06-17 14:13:58 -0400

    m

--- python/main.py
+++ python/main.py
@@ -5,25 +5,6 @@ from flask import Flask, render_template
 import func;
 #import sim;
 
-import Adafruit_SSD1306
-from PIL import Image, ImageDraw, ImageFont
-import RPi.GPIO as GPIO
-
-display = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_address=0x3C)
-# Setup
-display.begin()  # initialize graphics library for selected display module
-display.clear()  # clear display buffer
-display.display()  # write display buffer to physical display
-displayWidth = display.width  # get width of display
-displayHeight = display.height  # get height of display
-image = Image.new('1', (displayWidth, displayHeight))  # create graphics library image buffer
-draw = ImageDraw.Draw(image)  # create drawing object
-font = ImageFont.load_default()  # load and set default font
-
-# Draw text
-draw.text(((displayWidth - font.getsize("Woolsey")[0]) / 2, 0), "Woolsey", font=font, fill=255)  # center text at top of screen
-draw.text(((displayWidth - font.getsize("Workshop")[0]) / 2, 53), "Workshop", font=font, fill=255)  # center text at bottom of screen
-
 #func.print_test();
 
 app = Flask(__name__);
