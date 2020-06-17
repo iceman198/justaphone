6883f467c7f4756180960ea88620fee9ff72b0d7
Sean Dutton <iceman198@gmail.com>
2020-06-17 14:01:33 -0400

    m

--- python/main.py
+++ python/main.py
@@ -13,6 +13,34 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import subprocess
 
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+disp.begin()
+disp.clear()
+disp.display()
+
+width = disp.width
+height = disp.height
+
+image1 = Image.new('1', (width, height))
+
+draw = ImageDraw.Draw(image1)
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+padding = -2
+top = padding
+
+bottom = height-padding
+x = 0
+font = ImageFont.load_default()
+
+disp.clear()
+disp.display()
+draw.text((x, top),       "OLED Interfacing " ,  font=font, fill=255)
+draw.text((x, top+8),     "Circuit Digest", font=font, fill=255)
+draw.text((x, top+16),    "For more Videos",  font=font, fill=255)
+draw.text((x, top+25),    "Visit at",  font=font, fill=255)
+draw.text((x, top+34),    "www.circuitdigest.com",  font=font, fill=255)
+
 #func.print_test();
 
 app = Flask(__name__);
@@ -41,31 +69,3 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0');
 
     RST = 0
-
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-disp.begin()
-disp.clear()
-disp.display()
-
-width = disp.width
-height = disp.height
-
-image1 = Image.new('1', (width, height))
-
-draw = ImageDraw.Draw(image1)
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-padding = -2
-top = padding
-
-bottom = height-padding
-x = 0
-font = ImageFont.load_default()
-
-disp.clear()
-disp.display()
-draw.text((x, top),       "OLED Interfacing " ,  font=font, fill=255)
-draw.text((x, top+8),     "Circuit Digest", font=font, fill=255)
-draw.text((x, top+16),    "For more Videos",  font=font, fill=255)
-draw.text((x, top+25),    "Visit at",  font=font, fill=255)
-draw.text((x, top+34),    "www.circuitdigest.com",  font=font, fill=255)
