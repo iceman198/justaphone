3914a6fd58448bc7bf45b3a4053d1d16b9b55604
Sean Dutton <iceman198@gmail.com>
2020-09-18 15:00:07 -0400

    initial commit for the keypad

---
+++ Arduino/board-input/src/main.cpp
@@ -0,0 +1,30 @@
+#include "Keypad.h"
+
+const byte KP_ROWS = 4; //four rows
+const byte KP_COLS = 4; //three columns
+char keys[KP_ROWS][KP_COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte kpRowPins[KP_ROWS] = {12, 11, 10, 9}; //connect to the row pinouts of the keypad
+byte kpColPins[KP_COLS] = {8, 7, 6, 5}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad(makeKeymap(keys), kpRowPins, kpColPins, KP_ROWS, KP_COLS);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char key = keypad.getKey();
+
+  if (key != NO_KEY) {
+    Serial.print(key);
+    delay(100);
+  }
+}
