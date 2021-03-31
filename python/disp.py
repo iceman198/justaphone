80640997bec70cc7bb419865bb32df3a0c70709d
Sean Dutton <iceman198@gmail.com>
2021-03-31 09:55:05 -0400

    m

--- python/disp.py
+++ python/disp.py
@@ -8,7 +8,7 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 
 import logging;
-import epd2in13_V2;
+from . import epd2in13_V2;
 import time;
 from PIL import Image,ImageDraw,ImageFont;
 import traceback;
