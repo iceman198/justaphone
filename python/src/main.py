ed043a4b68f504eef1644a4ddb4f4ec856a76f09
Sean Dutton <iceman198@gmail.com>
2021-04-01 09:39:58 -0400

    m

--- python/src/main.py
+++ python/src/main.py
@@ -23,13 +23,9 @@ currentLine1 = "";
 currentLine2 = "";
 
 def keyboard_interrupt_handler(signal, frame):
-    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal));
-    disp.cleanup();
-    exit(0);
 
 #func.print_test();
 disp.init_display();
-signal.signal(signal.SIGINT, keyboard_interrupt_handler);
 
 app = Flask(__name__);
 
@@ -67,17 +63,23 @@ def nametest(name):
     return render_template('name.html', name=name);
 
 def myloop():
-    global doLoop, isRunning;
-    global currentStats, currentLine1, currentLine2
-    #print('isRunning = ' + str(isRunning)); #something isn't working right here but right now I don't care
-    if isRunning == False:
-        isRunning = True;
-        i = 0;
-        while doLoop:
-            #print('looping...' + str(i));
-            i = i + 1;
-            disp.update_disp(currentStats, currentLine1, currentLine2);
-            time.sleep(1);
+    try:
+        global doLoop, isRunning;
+        global currentStats, currentLine1, currentLine2
+        #print('isRunning = ' + str(isRunning)); #something isn't working right here but right now I don't care
+        if isRunning == False:
+            isRunning = True;
+            i = 0;
+            while doLoop:
+                #print('looping...' + str(i));
+                i = i + 1;
+                disp.update_disp(currentStats, currentLine1, currentLine2);
+                time.sleep(1);
+    except KeyboardInterrupt:    
+        print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal));
+        disp.cleanup();
+        exit(0);
+        #epd2in13_V2.epdconfig.module_exit()
 
 def start_flask():
     print('Flask Running...');
