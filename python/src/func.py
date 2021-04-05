1cb787ddb2f9fda3d23a7a04b8f6f16ef9f9b68d
Sean Dutton <iceman198@gmail.com>
2021-04-05 17:02:44 -0400

    m

--- python/src/func.py
+++ python/src/func.py
@@ -4,5 +4,4 @@ def print_test():
     print('This is a test');
 
 def log(file, service, text):
-    mytime = time.strftime('%a, %d %b %Y %H:%M:%S GMT' + time.localtime());
-    print(str(file) + '.' + str(mytime) + '() ~ ' + str(text));
+    print(str(file) + '.' + str(time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime())) + '() ~ ' + str(text));
