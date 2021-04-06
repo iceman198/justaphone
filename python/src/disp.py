b28923b1d6648bb469bce8453905a7a5e66b8694
Sean Dutton <iceman198@gmail.com>
2021-04-06 13:58:21 -0400

    m

--- python/src/disp.py
+++ python/src/disp.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import sys
-import os
+import sys;
+import os;
+import datetime;
+
+from waveshare_epd import epd2in13_V2;
+from PIL import Image,ImageDraw,ImageFont;
+
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir);
 
-import logging;
-from waveshare_epd import epd2in13_V2;
-import time;
-from PIL import Image,ImageDraw,ImageFont;
-import traceback;
-
-logging.basicConfig(level=logging.DEBUG);
-
 epd, font15, font24, time_image, time_draw = None, None, None, None, None;
 
 def init_display():
-    logging.info("disp.initDisplay() ~ init and Clear");
     global epd, font15, font24, time_image, time_draw;
     epd = epd2in13_V2.EPD();
     epd.init(epd.FULL_UPDATE);
@@ -41,20 +37,16 @@ def update_disp(mystats, textLine1, textLine2):
         time_draw.text((0, 40), textLine1, font = font24, fill = 0);
         time_draw.text((0, 80), textLine2, font = font24, fill = 0);
 
-        time_draw.text((180, 0), time.strftime('%H:%M:%S'), font = font15, fill = 0);
+        time_draw.text((170, 0), datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), font = font15, fill = 0);
         time_draw.text((0, 0), mystats[0], font = font15, fill = 0);
         time_draw.text((60, 0), mystats[1], font = font15, fill = 0);
         time_draw.text((120, 0), mystats[2], font = font15, fill = 0);
 
         epd.displayPartial(epd.getbuffer(time_image));
-        #logging.info("disp.updateDisp() ~ Goto Sleep...");
-        #epd.sleep();
-            
     except IOError as e:
-        logging.info("disp.updateDisp() ~ " & e);
+        i = 5 + 2;
 
 
 def cleanup():
     epd.Clear(0xFF);
-    logging.info("disp.cleanup() ~ Goto Sleep...");
     epd.sleep();
