4279600755e210850b3c576904dc25147de22b32
Sean Dutton <iceman198@gmail.com>
2021-04-06 09:04:33 -0400

    m

--- python/src/func.py
+++ python/src/func.py
@@ -4,13 +4,7 @@ def print_test():
     print('This is a test');
 
 def log(file, service, text):
-    #f = open("/home/pi/justaphone/python/phone.log", "a");
     mystring = str(time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime())) + ' *** ' + str(service) + '() ~ ' + str(text);
-    
-    #if "send_at" in str(service):
-        # don't print
-    #    f.write(mystring + '\r\n');
-
     print(mystring);
 
 
