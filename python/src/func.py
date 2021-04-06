cee016f31d32f3db8ea5f1bdeeaf4a5f9e6e9d6c
Sean Dutton <iceman198@gmail.com>
2021-04-06 13:43:46 -0400

    m

--- python/src/func.py
+++ python/src/func.py
@@ -1,9 +1,12 @@
 import time;
 
+f = open("/home/pi/justaphone/python/phone.log", "a");
+
 def print_test():
     print('This is a test');
 
 def log(file, service, text):
+    global f;
     mystring = str(time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime())) + ' *** ' + str(file) + ' *** ' + str(service) + '() ~ ' + str(text);
     print(mystring);
-
+    f.write(mystring+'\r\n');
