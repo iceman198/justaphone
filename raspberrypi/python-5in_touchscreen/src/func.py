2e73114d721aac7aab067b5c1b840667655ecb06
Sean Dutton <iceman198@gmail.com>
2021-08-16 12:40:17 -0400

    m

--- raspberrypi/python-5in_touchscreen/src/func.py
+++ raspberrypi/python-5in_touchscreen/src/func.py
@@ -4,7 +4,7 @@ def print_test():
     print('This is a test');
 
 def log(file, service, text):
-    f = open("/home/pi/justaphone/python/phone.log", "a");
+    f = open("./phone.log", "a");
     now = datetime.datetime.now();
     mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ' *** ' + str(file) + ' *** ' + str(service) + '() ~ ' + str(text);
     print(mystring);
