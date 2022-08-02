fe3dd9260ebb37a7b5afe130d5b17658789ee3cc
Sean Dutton <iceman198@gmail.com>
2022-08-02 10:16:36 -0400

    m

--- raspberrypi/python-nokia-display/runme.sh
+++ raspberrypi/python-nokia-display/runme.sh
@@ -2,7 +2,7 @@ cd /home/pi/justaphone/
 #git pull > /home/pi/justaphone/gitupdate.log
 git pull
 
-cd /home/pi/justaphone/raspberrypi/python-2.13touchscreen/src/
+cd /home/pi/justaphone/raspberrypi/python-nokia-display/src/
 rm *.pyc
 #python main.py > /home/pi/justaphone/phone_python.log
 python main.py
